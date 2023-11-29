604f0a6c8edb61312859e4d0ab6a489310d106f8
Pushpajekumar <mk6358522@gmail.com>
2023-11-29 20:23:47 +0530

    first bundling of our code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-console.log(" 🟢 hello from webpack !");
-alert("🟢 Server is running here...!");
+import { run } from "./app/app";
+
+import { AlertService } from "./app/alert.service";
+import { ComponentService } from "./app/component.service";
+
+const alertService = new AlertService();
+const componentService = new ComponentService();
+
+run(alertService, componentService);
